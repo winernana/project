5c68b5027990fb01b724cdfa8cf23ab9fd076f9e
winer <495893694@qq.com>
2019-03-18 09:01:01 +0800

    注册

--- miya/app/views.py
+++ miya/app/views.py
@@ -1,8 +1,14 @@
+import hashlib
+import random
+import time
+
+from django.core.cache import cache
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-# Create your views here.
-from app.models import Wheel, Shop, Goods
+
+
+from app.models import Wheel, Shop, Goods, User
 
 
 def home(request):
@@ -25,8 +31,40 @@ def login(request):
     return render(request,'login.html')
 
 
+def generate_password(param):
+    md5 = hashlib.md5()
+    md5.update(param.encode('utf-8'))
+    return md5.hexdigest()
+
+
+def generate_token():
+    temp = str(time.time()) + str(random.random())
+    md5 = hashlib.md5()
+    md5.update(temp.encode('utf-8'))
+    return md5.hexdigest()
+
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'GET':
+        return render(request,'register.html')
+    elif request.method == 'POST':
+        tel = request.POST.get('tel')
+        password = generate_password(request.POST.get('password'))
+
+        # 存入数据库
+        user = User()
+        user.tel = tel
+        user.password = password
+        user.save()
+
+        # 状态保持
+        token =generate_token()
+        # token 和用户具有一一对应关系
+        # key --> value -->过期时间
+        cache.set(token,user.id ,60*60*24*3 )
+        request.session['token'] = token
+
+        return  redirect('miya:home')
+
 
 
 def shoppingCar(request):
